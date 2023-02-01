eb7c2ef7795676f1c277c9fded7c7fbc068fce39
root <root@UTILISA-UUJQHQA>
2023-02-01 09:13:10 +0100

    Ajout titre

--- src/Application.java
+++ src/Application.java
@@ -16,7 +16,7 @@ public class Application extends Frame implements WindowListener {
 		Vue vue = new Vue();
 		modl.addObserver(vue);
 		vue.addActionListener(ctrl);
-		this.setTitle("tutoriel git");
+		this.setTitle("tutoriel githuub");
 		this.add(vue);
 		this.pack();
 		this.setVisible(true);
